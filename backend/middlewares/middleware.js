f75a28d60945cc23e3bf64962d8bfe33a71202e9
mohanNaryana <mohankrishnateja2022@gmail.com>
2024-10-26 19:33:23 +0530

    changed export in authenticate folder

--- backend/middlewares/middleware.js
+++ backend/middlewares/middleware.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 const config = require('../config')
 
 
-export async function  authenticateToken(req,res,next){
+async function  authenticateToken(req,res,next){
     const token = req.headers["authorization"]
     if (!token) {
         return res.status(401).json({
